2a5e9f1db965c4c9e2b34af42f584eb6c548bf6b
BKaperick <bryankaperick@gmail.com>
2017-03-12 03:42:07 -0400

    Set up scraper to begin crawling.

--- scrape_WaPo.py
+++ scrape_WaPo.py
@@ -1,10 +1,12 @@
 from lxml import html
 import requests
+import pickle
+import sys
 
 '''All news articles on Washington Post's website have a url
 "https://www.washingtonpost.com/[topic]/..."
 '''
-WaPo_topics = {'world', 'politics', 'news'}
+WaPo_topics = {'world', 'politics', 'news', 'investigation', 'local'}
 
 
 def get_article_data(url):
@@ -34,6 +36,23 @@ def get_article_data(url):
 
     return article_text, suggested
 
+def name_from_url(url):
+    '''
+    Extracts a human-readable name to serve as an identifier for each
+    unique article.  The urls seem like they may be subject to change.
+    '''
+    url_data = url.split('.com/')[1]
+    url_data = url_data.split('/')
+    for i,chunk in enumerate(url_data):
+        if len(chunk) == 4 and len(url_data[i+1]) == 2 and len(url_data[i+2]) == 2:
+            if '.html' in url_data[i+3]:
+                name = url_data[i-1]
+            else:
+                name = url_data[i+3]
+            name.replace('-','_')
+            return name
+    
+
 # Get list of articles currently on the front page.
 r = requests.get('https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=bf235085e456486c962c8afc4b59cd6e')
 success = str(r.status_code)
@@ -43,14 +62,33 @@ articles = r.json()['articles']
 
 # Keep track of already-scraped articles
 visited = set()
+counter = 0
+
+network = dict()
 
 article_urls = [art['url'] for art in articles]
 for url in article_urls:
-    print(url)
-    visited.add(url)
+    
+    # Scrate data
     text, suggested = get_article_data(url)
+    
+    # Update information
+    name = name_from_url(url)
+    network[name] = suggested
+    visited.add(name)
+    
+    # Save text to file
+    with open(name + '.txt','w') as f:
+        f.write(text)
 
     # Add to queue the list of urls which have been linked but not already visited
     unexplored = list(suggested - visited)
     article_urls += unexplored
-
+    
+    # Periodically save state of scraping
+    counter += 1
+    print(counter)
+    if counter % 10 == 0:
+        pickle.dump(visited, file('visited.pkl','wb'))
+        pickle.dump(article_urls, file('queue.pkl','wb'))
+        pickle.dump(network, file('adj_list.pkl','wb'))
