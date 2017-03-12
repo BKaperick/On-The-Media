fbd6c3ac62dc92416dec1c4f743a0e216596252c
BKaperick <bryankaperick@gmail.com>
2017-03-12 03:22:00 -0400

    Fixed up some typos, clarified the code and documented everything so-far implemented.

--- scrape_WaPo.py
+++ scrape_WaPo.py
@@ -1,6 +1,9 @@
 from lxml import html
 import requests
 
+'''All news articles on Washington Post's website have a url
+"https://www.washingtonpost.com/[topic]/..."
+'''
 WaPo_topics = {'world', 'politics', 'news'}
 
 
@@ -19,30 +22,35 @@ def get_article_data(url):
     article_text = ''.join(article_raw)
 
     suggested = set()
-    # Skips over some nodes in final depth search
-    #xpath_str = '/html/body/div[@id="pb-root"]/section[@id="main-content"]/div[@class="moat-trackable pb-f-theme-normal full pb-feature pb-layout-item pb-f-page-recommended-strip"]/div[@id="post-recommends"]/div[@id="slug_postrecommends"]'
     
-    #xpath_str = '/html/body/div[@id="pb-root"]/section[@id="main-content"]/div[@class="moat-trackable pb-f-theme-normal full pb-feature pb-layout-item pb-f-page-recommended-strip"]/div[@id="post-recommends"]/div[@class="postrecommends contentfeed"]/div[@class="content-strip"]/div[4]/div/div/div[2]/div[2]/a'
-    #xpath_str = '////div[class="content-strip"]'
+    # 'a' nodes seem to hold all external links
     xpath_str = '/html/body/div[@id="pb-root"]/section[@id="main-content"]//a'
-    sugg = tree.xpath(xpath_str)
-    for sugg_obj in sugg:
-        sugg_url = sugg_obj.get('href')
-        if sugg_url:
-            if any(['washingtonpost.com/' + x + '/' in sugg_url for x in WaPo_topics]):
-                suggested.add(sugg_url)
+    for node in tree.xpath(xpath_str):
+        node_url = node.get('href')
+        if node_url:
+            # Only return internal links to WaPo news articles.
+            if any(['washingtonpost.com/' + topic + '/' in node_url for topic in WaPo_topics]):
+                suggested.add(node_url)
 
     return article_text, suggested
 
+# Get list of articles currently on the front page.
 r = requests.get('https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=bf235085e456486c962c8afc4b59cd6e')
-print(r.status_code)
+success = str(r.status_code)
+if success != '200':
+    raise Exception('error code ' + success)
 articles = r.json()['articles']
 
+# Keep track of already-scraped articles
 visited = set()
-art = articles[0]
-#for art in articles:
-url = art['url']
-visited.add(url)
-text, suggested = get_article_data(url)
-#print(text)
-#print(suggested)
+
+article_urls = [art['url'] for art in articles]
+for url in article_urls:
+    print(url)
+    visited.add(url)
+    text, suggested = get_article_data(url)
+
+    # Add to queue the list of urls which have been linked but not already visited
+    unexplored = list(suggested - visited)
+    article_urls += unexplored
+
