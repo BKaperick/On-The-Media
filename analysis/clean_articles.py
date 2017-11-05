5d2aff73bac67c99259189e999ebfbb4322d4c12
bkaperick <bryankaperick@gmail.com>
2017-11-05 16:05:48 -0500

    Updated analysis a bit.  Experimentation with analogies.

--- analysis/clean_articles.py
+++ analysis/clean_articles.py
@@ -8,21 +8,26 @@ def clean_str(s):
 
 article_dir = '../articles/'
 
-used = set()
-with open('articles_used.txt', 'r') as history_file:
-    for line in history_file.readlines():
-        used.add(line.strip())
+def clean():
+    # Create set containing used articles (to avoid duplicates)
+    used = set()
+    with open('articles_used.txt', 'r') as history_file:
+        for line in history_file.readlines():
+            used.add(line.strip())
 
-used_update = set()
 
-with open('combined','a') as combined_file:
-    for f in os.listdir(article_dir):
-        if f[-4:] == '.txt' and f not in used:
-            with open(article_dir + f, 'r') as art:
-                for line in art.readlines():
-                    combined_file.write(clean_str(line))
-            used_update.add(f)
+    used_update = set()
+    with open('combined','a') as combined_file:
+        for f in os.listdir(article_dir):
+            if f[-4:] == '.txt' and f not in used:
 
-with open('articles_used.txt', 'a') as history_file:
-    for article in used_update:
-        history_file.write(article + '\n')
+                # Add article contents to 'combined'
+                with open(article_dir + f, 'r') as art:
+                    for line in art.readlines():
+                        combined_file.write(clean_str(line))
+                used_update.add(f)
+
+    print("{0} new articles added to collection.".format(len(used_update)))
+    with open('articles_used.txt', 'a') as history_file:
+        for article in used_update:
+            history_file.write(article + '\n')
--- analysis/test.py
+++ analysis/test.py
@@ -1,18 +1,50 @@
 import word2vec as w2v
+import clean_articles
 from pprint import pprint
+import sys
 
-#w2v.word2phrase('./text8', './text8-phrases', verbose=True) 
-w2v.word2phrase('combined', './text8-phrases', verbose=True) 
-w2v.word2vec('text8-phrases', 'text8.bin', size=100, verbose=True)
+if len(sys.argv) > 1 and sys.argv[1] in ['-t','-train']:
+    
+    # Add new articles to file
+    clean_articles.clean()
 
-model = w2v.load('text8.bin')
+    # Train new model
+    w2v.word2phrase('combined', './text-phrases', verbose=True) 
+    w2v.word2vec('text8-phrases', 'text.bin', size=100, verbose=True)
+    w2v.word2clusters('combined', 'text-clusters.txt', 100, verbose=True)
+
+# Initialize pre-trained model
+model_old = w2v.load('text8.bin')
+model = w2v.load('text.bin')
+clusters = w2v.load_clusters('text-clusters.txt')
+model.clusters = clusters
+
+#ind = clusters['Trump']
+#print(clusters.get_words_on_cluster(ind))
+print(len(model_old.vocab))
+print(len(model.vocab))
+
+# King - man + woman : "Man is to King as Woman is to
+# Trump - America + Germany
+pos=['Putin','America']
+neg=['Russia']
+leader = model.analogy(pos,neg)
+
+print()
+print("{0} is to {1} as {2} is to ...".format(neg[0], pos[0], pos[1])) 
+print(model.generate_response(*leader))
 
 
-while True:
-    word = input()
-    indexes, metrics = model.cosine(word)
-    model.vocab[indexes]
-    print("\n")
-
-    for word,dist in model.generate_response(indexes, metrics).tolist():
-        print(word,dist)
+#while True:
+#    print("\n")
+#    word = input()
+#    try:
+#        print(model.cosine(word))
+#        indexes, metrics = model.cosine(word)
+#        #model.vocab[indexes]
+#        print(model.generate_response(indexes, metrics).tolist())
+#        for word,dist,size in model.generate_response(indexes, metrics).tolist():
+#            print(word,dist,size)
+#
+#    except KeyError:
+#        pass
